3b6f52e52219f6d9e73a6f2bb855b8b087f30009
Paulina Michel <Paulina Michel@MICHI>
2024-11-04 21:39:08 -0700

    Agregar archivos de proyecto.

---
+++ DiasSemanaArreglos/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace DiasSemanaArreglos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce un numero");
+            string[] diasSemana = new string[7];
+            diasSemana[0] = "Lunes";
+            diasSemana[1] = "Martes";
+            diasSemana[2] = "Miercoles";
+            diasSemana[3] = "Jueves";
+            diasSemana[4] = "Viernes";
+            diasSemana[5] = "Sabado";
+            diasSemana[6] = "Domingo";
+            
+            string input = Console.ReadLine();
+            int.TryParse(input, out int result);
+
+            if (result > 0 && result <= 7)
+            {
+                Console.WriteLine(diasSemana[result - 1]);
+            }
+            else 
+            {
+                Console.WriteLine("Dato Incorrecto");
+            }
+        }
+    }
+}
